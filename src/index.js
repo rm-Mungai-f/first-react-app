a4676f789de366f9cd9490581c5f574024c43310
rm-Mungai-f <mungai.mbugua1409@gmail.com>
2023-01-24 12:35:14 +0300

    demonstrate useState react hook

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 
+// const App = () =>{
+//     return (
+//         <div>
+//             Hello World
+//         </div>
+//     )
+// }
+
+//function App() {
 const App = () =>{
+    const [counter, setCounter] = useState(0);
     return (
         <div>
-            Hello World
+            <p>
+                You clicked {counter} times
+            </p>
+            <button onClick={() => setCounter(counter + 1)}>
+                Increment by 1
+            </button>
+            <button onClick={() => setCounter(counter - 1)}>
+                Decrement by 1
+            </button>
         </div>
-    )
+    );
 }
+
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
